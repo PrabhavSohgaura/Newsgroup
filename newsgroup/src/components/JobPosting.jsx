827d9762e3e9c4ad3fb3c0c255a22f34e2378b9c
PrabhavSohgaura <prabhavsohgaura333@gmail.com>
2023-12-19 17:06:09 +0530

    added the structure

--- newsgroup/src/components/JobPosting.jsx
+++ newsgroup/src/components/JobPosting.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const JobPosting = () => {
-  return <div>JobPosting</div>;
+  return (
+    <div>
+      <h1>title</h1>
+      <p>para</p>
+    </div>
+  );
 };
 
 export default JobPosting;
